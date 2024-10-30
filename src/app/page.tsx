14e783bf9b6223f250d6c235ab0fd36c6ee4be46
Zhen Zhong <auroradysis@gmail.com>
2024-10-30 00:31:29 -0400

    first version from http://incaseofstairs.com/jsdiff/

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,180 @@
-import Image from "next/image";
+"use client";
+
+import React, { useState, useEffect } from "react";
+import {
+  Change,
+  createTwoFilesPatch,
+  diffChars,
+  diffWords,
+  diffLines,
+} from "diff";
+
+interface ChangeWithChunkHeader extends Change {
+  chunkHeader: boolean;
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [diffType, setDiffType] = useState("diffChars");
+  const [textA, setTextA] = useState("restaurant");
+  const [textB, setTextB] = useState("aura");
+  const [diffResult, setDiffResult] = useState<ChangeWithChunkHeader[]>([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+  useEffect(() => {
+    computeDiff();
+  }, [textA, textB, diffType]);
+
+  const computeDiff = () => {
+    let diff: ChangeWithChunkHeader[];
+    if (diffType === "diffPatch") {
+      let pastHunkHeader = false;
+      const patch = createTwoFilesPatch("a.txt", "b.txt", textA, textB)
+        .split("\n")
+        .map((entry) => {
+          const result = {
+            value: entry + "\n",
+            chunkHeader: false,
+            removed: false,
+            added: false,
+          };
+          if (entry.startsWith("@@")) {
+            result.chunkHeader = true;
+            pastHunkHeader = true;
+          } else if (pastHunkHeader) {
+            if (entry.startsWith("-")) {
+              result.removed = true;
+            } else if (entry.startsWith("+")) {
+              result.added = true;
+            }
+          }
+          return result;
+        });
+      diff = patch;
+    } else if (diffType === "diffChars") {
+      diff = diffChars(textA, textB).map((entry) => {
+        return {
+          ...entry,
+          chunkHeader: false,
+        };
+      });
+    } else if (diffType === "diffWords") {
+      diff = diffWords(textA, textB).map((entry) => {
+        return {
+          ...entry,
+          chunkHeader: false,
+        };
+      });
+    } else {
+      diff = diffLines(textA, textB).map((entry) => {
+        return {
+          ...entry,
+          chunkHeader: false,
+        };
+      });
+    }
+
+    // Swap logic
+    for (let i = 0; i < diff.length; i++) {
+      if (diff[i].added && diff[i + 1] && diff[i + 1].removed) {
+        const swap = diff[i];
+        diff[i] = diff[i + 1];
+        diff[i + 1] = swap;
+      }
+    }
+
+    setDiffResult(diff);
+  };
+
+  const handleDiffTypeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDiffType(e.target.value);
+  };
+
+  return (
+    <div>
+      <div id="settings">
+        <h1>Diff</h1>
+        <label>
+          <input
+            type="radio"
+            value="diffChars"
+            checked={diffType === "diffChars"}
+            onChange={handleDiffTypeChange}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          Chars
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="diffWords"
+            checked={diffType === "diffWords"}
+            onChange={handleDiffTypeChange}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+          Words
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="diffLines"
+            checked={diffType === "diffLines"}
+            onChange={handleDiffTypeChange}
           />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Lines
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="diff_type"
+            value="diffPatch"
+            checked={diffType === "diffPatch"}
+            onChange={handleDiffTypeChange}
+          />
+          Patch
+        </label>
+      </div>
+
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <textarea
+                value={textA}
+                onChange={(e) => setTextA(e.target.value)}
+                spellCheck="false"
+                rows={10}
+                cols={30}
+              />
+            </td>
+            <td>
+              <textarea
+                value={textB}
+                onChange={(e) => setTextB(e.target.value)}
+                rows={10}
+                cols={30}
+              />
+            </td>
+            <td>
+              <div>
+                <pre id="result">
+                  {diffResult.map((part, index) => {
+                    if (part.removed) {
+                      return <del key={index}>{part.value}</del>;
+                    } else if (part.added) {
+                      return <ins key={index}>{part.value}</ins>;
+                    } else if (part.chunkHeader) {
+                      return (
+                        <span key={index} className="chunk-header">
+                          {part.value}
+                        </span>
+                      );
+                    } else {
+                      return <span key={index}>{part.value}</span>;
+                    }
+                  })}
+                </pre>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }

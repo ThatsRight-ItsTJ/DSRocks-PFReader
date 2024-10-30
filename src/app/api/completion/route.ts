d8a04b7b47440e8fb4dd61aa45baf2968a2a036c
Zhen Zhong <auroradysis@gmail.com>
2024-10-30 17:20:01 +0000

    dev

---
+++ src/app/api/completion/route.ts
@@ -0,0 +1,21 @@
+import { createOpenAI } from "@ai-sdk/openai";
+import { streamText, CoreMessage } from "ai";
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 500;
+
+export async function POST(req: Request) {
+  const { model, prompt }: { model: string; prompt: string } = await req.json();
+
+  const openai = createOpenAI({
+    baseURL: process.env.OPENAI_BASE_URL,
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const result = await streamText({
+    model: openai(model),
+    prompt,
+  });
+
+  return result.toDataStreamResponse();
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,64 @@
 "use client";
 
+import { useAtom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 import { useRef } from "react";
+import { useCompletion } from "ai/react";
 import { Button, NextUIProvider, Select, SelectItem } from "@nextui-org/react";
-import { DiffEditor, MonacoDiffEditor, Monaco } from "@monaco-editor/react";
+import { DiffEditor, MonacoDiffEditor } from "@monaco-editor/react";
+
+const modelAtom = atomWithStorage("model", "anthropic/claude-3.5-sonnet");
+const contextAtom = atomWithStorage("modelContext", "academic");
+const instructionAtom = atomWithStorage("instruction", "basicProofread");
+const textOriginalEditorAtom = atomWithStorage(
+  "textOriginalEditor",
+  "the original text"
+);
+const textModifiedEditorAtom = atomWithStorage(
+  "textModifiedEditor",
+  "the modified text"
+);
 
 export default function Home() {
   const editorRef = useRef<MonacoDiffEditor | null>(null);
+  const [model, setModel] = useAtom(modelAtom);
+  const [context, setContext] = useAtom(contextAtom);
+  const [instruction, setInstruction] = useAtom(instructionAtom);
+  const [originalText, setOriginalText] = useAtom(textOriginalEditorAtom);
+  const [modifiedText, setModifiedText] = useAtom(textModifiedEditorAtom);
 
-  const handleEditorDidMount = (editor: MonacoDiffEditor, monaco: Monaco) => {
+  const handleEditorDidMount = (editor: MonacoDiffEditor) => {
     editorRef.current = editor;
+    editor.getOriginalEditor().onDidChangeModelContent((event) => {
+      setOriginalText(editor.getOriginalEditor().getValue());
+    });
+    editor.getModifiedEditor().onDidChangeModelContent((event) => {
+      setModifiedText(editor.getModifiedEditor().getValue());
+    });
   };
 
-  const handleSubmit = () => {
+  const { completion, complete } = useCompletion({
+    initialCompletion: modifiedText,
+  });
+  setModifiedText(completion);
+
+  const handleProofread = async () => {
     const editor = editorRef.current;
     if (editor) {
-      const value = editor.getOriginalEditor().getValue();
-      console.log(value);
-      editor.getModifiedEditor().setValue(value + "\n// modified");
+      await complete(instruction + ":\n" + originalText, {
+        body: {
+          model: model,
+        },
+      });
     }
   };
+
+  const models = [
+    "anthropic/claude-3.5-sonnet",
+    "anthropic/claude-3-opus",
+    "openai/chatgpt-4o-latest",
+    "openai/gpt-4",
+  ];
 
   const contexts = [
     { key: "academic", label: "Academic" },
@@ -27,25 +67,98 @@ export default function Home() {
     { key: "oral", label: "Oral" },
   ];
 
+  const instructions = [
+    {
+      key: "basicProofread",
+      description: "最基本的校稿指令",
+      prompt: "Proofread this text",
+    },
+    {
+      key: "awkwardParts",
+      description:
+        "仅作些许编辑, 修正非英语母语人士常犯的错误, 包括用词、语法和逻辑",
+      prompt: "Fix only awkward parts",
+    },
+    {
+      key: "streamline",
+      description: "精简和梳理不通顺之处, 使整体内容更清晰",
+      prompt: "Streamline any awkward words or phrases",
+    },
+    {
+      key: "polish",
+      description: "更积极地编辑和润饰, 修改程度更高",
+      prompt: "Polish any awkward words or phrases",
+    },
+    {
+      key: "trim",
+      description: "如果文本太过冗长",
+      prompt: "Trim the fat",
+    },
+    {
+      key: "clarityAndFlow",
+      description: "提高清晰度和流畅性",
+      prompt: "Improve clarity and flow",
+    },
+    {
+      key: "significantClarityAndFlow",
+      description: "显著提高清晰度和流畅性",
+      prompt: "Significantly improving clarity and flow",
+    },
+  ];
+
   return (
     <NextUIProvider>
-      <div className="flex-col items-center justify-center">
-        <div className="flex items-center justify-center">
-          <Select label="Select a context" className="max-w-xs p-4">
+      <div className="flex-col items-center justify-center h-screen">
+        <div className="flex items-center justify-center h-10 mt-4 gap-4">
+          <Select
+            label="Select a model"
+            selectedKeys={[model]}
+            onSelectionChange={(keys) =>
+              keys.currentKey && setModel(keys.currentKey)
+            }
+            className="max-w-64"
+          >
+            {models.map((model) => (
+              <SelectItem key={model}>{model}</SelectItem>
+            ))}
+          </Select>
+          <Select
+            label="Select a context"
+            selectedKeys={[context]}
+            onSelectionChange={(keys) =>
+              keys.currentKey && setContext(keys.currentKey)
+            }
+            className="max-w-36"
+          >
             {contexts.map((context) => (
               <SelectItem key={context.key}>{context.label}</SelectItem>
             ))}
           </Select>
-          <Button className="ml-4" onPress={handleSubmit}>
-            Submit
-          </Button>
+          <Select
+            label="Select an instruction"
+            selectedKeys={[instruction]}
+            onSelectionChange={(keys) =>
+              keys.currentKey && setInstruction(keys.currentKey)
+            }
+            className="max-w-md"
+          >
+            {instructions.map((instruction) => (
+              <SelectItem
+                key={instruction.key}
+                description={instruction.description}
+              >
+                {instruction.prompt}
+              </SelectItem>
+            ))}
+          </Select>
+          <Button onPress={handleProofread}>Proofread</Button>
         </div>
         <DiffEditor
-          className="h-screen"
+          className="h-full mt-4"
           language="plaintext"
-          options={{ originalEditable: true }}
-          original="// the original code"
-          modified="// the modified code"
+          options={{ originalEditable: true, wordWrap: "on" }}
+          original={originalText}
+          modified={modifiedText}
           onMount={handleEditorDidMount}
         />
       </div>

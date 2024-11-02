1fb40a96199cb7982daed4692a2ce44149b8f141
Zhen Zhong <auroradysis@gmail.com>
2024-11-02 12:10:27 +0000

    Update prompt.ts

--- src/lib/prompt.ts
+++ src/lib/prompt.ts
@@ -3,68 +3,10 @@ export const models = process.env.NEXT_PUBLIC_OPENAI_MODEL?.split(
 ) as string[];
 
 export const contexts = [
-  { key: "academic", label: "Academic" },
-  { key: "general", label: "General" },
-  { key: "instantMessage", label: "Instant Message" },
-  { key: "email", label: "Email" },
-  { key: "oral", label: "Oral" },
-  { key: "gitCommitMessage", label: "Git Commit Message" },
-];
-
-export const instructions = [
   {
-    key: "basicProofread",
-    prompt: "Proofreading the text",
-  },
-  {
-    key: "awkwardParts",
-    prompt: "Fixing only awkward parts",
-  },
-  {
-    key: "streamline",
-    prompt: "Streamlining any awkward words or phrases",
-  },
-  {
-    key: "polish",
-    prompt: "Polishing any awkward words or phrases",
-  },
-  {
-    key: "trim",
-    prompt: "Triming the fat",
-  },
-  {
-    key: "clarityAndFlow",
-    prompt: "Improving clarity and flow",
-  },
-  {
-    key: "significantClarityAndFlow",
-    prompt: "Significantly improving clarity and flow",
-  },
-];
-
-export function generate_system_prompt(
-  contextKey: string,
-  instructionKey: string
-) {
-  // Lookup the context and instruction from the arrays
-  const context = contexts.find((c) => c.key === contextKey);
-  const instruction = instructions.find((i) => i.key === instructionKey);
-
-  if (!context) {
-    throw new Error("Invalid context key");
-  }
-  if (!instruction) {
-    throw new Error("Invalid instruction key");
-  }
-
-  let role =
-    "As an expert in English language arts with advanced experience in proofreading, editing, spelling, grammar, proper sentence structure, and punctuation, your task is to ensure the given text is error-free, clear, and effective in achieving its intended purpose. You have critical thinking skills with the ability to analyze and evaluate information, arguments, and ideas, and to make logical and well-supported judgments and decisions.";
-  const styleGuidelines: string[] = [];
-
-  // Define style guidelines based on the context
-  switch (context.key) {
-    case "academic":
-      role = `You are an elite-level editor and proofreader with over 20 years of experience in
+    key: "academic",
+    label: "Academic",
+    prompt: `You are an elite-level editor and proofreader with over 20 years of experience in
 academic and scientific writing across diverse fields.
 
 Your expertise is characterized by:
@@ -129,66 +71,108 @@ Please focus on the following aspects:
 Your task is only to focus on the language usage in the paper.
 - Do not proofread or comment on the scientific content of the paper.
 - Do not proofread or suggest improvements on the use of LaTeX.
-These tasks are handled by other experts.
+These tasks are handled by other experts.`,
+  },
+  {
+    key: "general",
+    label: "General",
+    prompt: `You are a versatile communicator, able to adapt your language and style to a wide variety of contexts and audiences.`,
+  },
+  {
+    key: "instantMessage",
+    label: "Instant Message",
+    prompt:
+      "You are a seasoned digital communicator, adept at conveying clear, concise, and effective messages in an instant messaging environment.",
+    guidelines: `- Using an informal tone appropriate for casual conversations.
+- Emojis and abbreviations are acceptable.
+- Making sure it's clear, concise, polite, and easy to understand.
+- Using emojis sparingly if necessary.`,
+  },
+  {
+    key: "email",
+    label: "Email",
+    prompt:
+      "You are an elite-level editor and proofreader with over 20 years of experience in email communication.",
+    guidelines: `- Employing a professional tone suitable for business emails.`,
+  },
+  {
+    key: "oral",
+    label: "Oral",
+    prompt:
+      "You are an experienced public speaker, able to articulate complex ideas clearly and persuasively in spoken language.",
+    guidelines: `- Using language that would sound natural when spoken aloud.
+- Avoiding complex sentence structures.`,
+  },
+  {
+    key: "gitCommitMessage",
+    label: "Git Commit Message",
+    prompt:
+      "You are a proficient software developer, skilled at writing concise, informative Git commit messages that effectively communicate the purpose of each change.",
+    guidelines: `- Treating provided text as a git commit message.
+- Ensuring it's formatted with a short summary line, followed by a blank line, followed by a more detailed description.
+- Ensuring that the summary line is less than 50 characters.
+- Ensuring that the lines in the description are less than 72 characters.
+- Avoiding using past tense, and use the imperative mood instead.`,
+  },
+];
 
-Reply only with the corrected text. Do not provide explanations.`;
-      break;
-    case "general":
-      styleGuidelines.push(
-        "Using a neutral tone appropriate for general audiences."
-      );
-      break;
-    case "instantMessage":
-      styleGuidelines.push(
-        ...[
-          "Using an informal tone appropriate for casual conversations.",
-          "Emojis and abbreviations are acceptable.",
-          "Making sure it's clear, concise, polite, and easy to understand.",
-          "Using emojis sparingly if necessary.",
-        ]
-      );
-      break;
-    case "email":
-      styleGuidelines.push(
-        "Employing a professional tone suitable for business emails."
-      );
-      break;
-    case "oral":
-      styleGuidelines.push(
-        ...[
-          "Using language that would sound natural when spoken aloud.",
-          "Avoiding complex sentence structures.",
-        ]
-      );
-      break;
-    case "gitCommitMessage":
-      styleGuidelines.push(
-        ...[
-          "Treating provided text as a git commit message.",
-          "Ensuring it's formatted with a short summary line, followed by a blank line, followed by a more detailed description.",
-          "Ensuring that the summary line is less than 50 characters.",
-          "Ensuring that the lines in the description are less than 72 characters.",
-          "Avoiding using past tense, and use the imperative mood instead.",
-        ]
-      );
-      break;
-    default:
-      break;
+export const instructions = [
+  {
+    key: "basicProofread",
+    prompt: "proofread the text",
+  },
+  {
+    key: "awkwardParts",
+    prompt: "fix only awkward parts",
+  },
+  {
+    key: "streamline",
+    prompt: "streamline any awkward words or phrases",
+  },
+  {
+    key: "polish",
+    prompt: "polish any awkward words or phrases",
+  },
+  {
+    key: "trim",
+    prompt: "trim the fat",
+  },
+  {
+    key: "clarityAndFlow",
+    prompt: "improve clarity and flow",
+  },
+  {
+    key: "significantClarityAndFlow",
+    prompt: "significantly improve clarity and flow",
+  },
+];
+
+export function generate_system_prompt(
+  contextKey: string,
+  instructionKey: string
+) {
+  // Lookup the context and instruction from the arrays
+  const context = contexts.find((c) => c.key === contextKey);
+  const instruction = instructions.find((i) => i.key === instructionKey);
+
+  if (!context) {
+    throw new Error("Invalid context key");
+  }
+  if (!instruction) {
+    throw new Error("Invalid instruction key");
   }
 
-  styleGuidelines.push(instruction.prompt + ".");
-
-  if (context.key === "academic") {
-    return role;
-  } else {
-    return `${role}
-
-Your approach should involve:
-${styleGuidelines.map((guideline) => `- ${guideline}`).join("\n")}
-- Carefully reading through the communication to identify any errors, inconsistencies, or areas where clarity could be improved.
-- Making appropriate updates to increase readability, professionalism, and cohesiveness.
-- Ensuring that the intended meaning is conveyed accurately.
-
-Reply only with the corrected text. Do not provide explanations.`;
+  let instruction_prompt = "";
+  if (context.key !== "academic") {
+    instruction_prompt = `\nYour task is to ${instruction.prompt}.\n`;
+    if (context.guidelines) {
+      instruction_prompt += `\nYour approach should involve:
+${context.guidelines}\n`;
+    }
   }
+
+  const prompt = `${context.prompt}
+${instruction_prompt}
+Reply only with the corrected text. Do not provide explanations.`;
+  return prompt;
 }
